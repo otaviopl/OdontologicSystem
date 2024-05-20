12ec9c4b901e72711ac0c18b3e08109c1b33e5ff
otaviopl <otavio.pereira.lopes@gmail.com>
2024-05-20 00:44:29 -0300

    FEAT: add editresource.js page

---
+++ src/pages/ViewResouces/ViewResources.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getResources, deleteResource } from '../../services/resourceService';
+import './ViewResources.css';
+
+function ViewResources() {
+  const resources = getResources();
+
+  const handleDelete = (id) => {
+    deleteResource(id);
+    window.location.reload();
+  };
+
+  return (
+    <div className="view-resources">
+      <h1>Pacientes Cadastrados</h1>
+      <ul>
+        {resources.map(resource => (
+          <li key={resource.id}>
+            {resource.name} - {resource.age} anos
+            <button onClick={() => handleDelete(resource.id)}>Excluir</button>
+            <Link to={`/edit/${resource.id}`}>Editar</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ViewResources;
