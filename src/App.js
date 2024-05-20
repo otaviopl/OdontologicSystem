f8443eee4ce3ffe12024f9d496050f4810e12dfa
otaviopl <otavio.pereira.lopes@gmail.com>
2024-05-20 00:56:44 -0300

    FIX: fix path of edit and view resources files.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
-import AddResource from './pages/AddResource/AddResource';
-import ViewResources from './pages/ViewResources/ViewResources';
-import EditResource from './pages/EditResource/EditResource';
+import AddResource from '../src/pages/AddResource/AddResource';
+import ViewResources from '../src/pages/ViewResouces/ViewResources';
+import EditResource from '../src/pages/EditResource/EditResouce';
 import './App.css';
 
 function App() {
