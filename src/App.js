34408abb044e40eb2fa01ca4c7e85fe3265e3bb9
otaviopl <otavio.pereira.lopes@gmail.com>
2024-05-20 01:03:15 -0300

    REFACTOR: rename EditResource files .. wrong name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
-import AddResource from '../src/pages/AddResource/AddResource';
-import ViewResources from '../src/pages/ViewResouces/ViewResources';
+import AddResource from '../src/pages/EditResource/EditResouce';
+import ViewResources from '../src/pages/EditResource/EditResouce';
 import EditResource from '../src/pages/EditResource/EditResouce';
 import './App.css';
 
@@ -14,12 +14,12 @@ function App() {
       <div className="App">
         <Header />
         <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/add" component={AddResource} />
-            <Route path="/view" component={ViewResources} />
-            <Route path="/edit/:id" component={EditResource} />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddResource />} />
+            <Route path="/view" element={<ViewResources />} />
+            <Route path="/edit/:id" element={<EditResource />} />
+          </Routes>
         </main>
         <Footer />
       </div>
@@ -28,3 +28,11 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+'../src/pages/EditResource/EditResouce';
--- src/pages/EditResource/EditResource.js
+++ src/pages/EditResource/EditResource.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { getResources, updateResource } from '../../services/resourceService';
-import './EditResource.css';
+
 
 function EditResource() {
   const { id } = useParams();
