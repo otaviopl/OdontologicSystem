52d5dbca7cf8aca76a920f1154c4221225417bc4
otaviopl <otavio.pereira.lopes@gmail.com>
2024-05-20 00:38:19 -0300

    FEAT: add new estructureto footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Consultório Dentário. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
